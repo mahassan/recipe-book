1d6b4f1d4be26881e9e988be265412a1bfe57251
Ammar <emaarkhan@gmail.com>
2017-11-23 16:52:21 +0000

    model properties

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe{
+    name:string;
+    descrition: string;
+    image:string;
+
+    constructor(name, desc, img){
+        this.name = name;
+        this.descrition = desc;
+        this.image = img;
+    }
+}
